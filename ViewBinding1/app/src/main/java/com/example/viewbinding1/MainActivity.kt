642fa91eddd299198e586b8b6f50e992dd14ad0f
FJ17-inchman <fjmrjr13@gmail.com>
2022-03-06 11:10:36 -0600

    Test p 1

--- ViewBinding1/app/src/main/java/com/example/viewbinding1/MainActivity.kt
+++ ViewBinding1/app/src/main/java/com/example/viewbinding1/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.viewbinding1.databinding.ActivityMainBinding
 
+//*Activity*//
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding

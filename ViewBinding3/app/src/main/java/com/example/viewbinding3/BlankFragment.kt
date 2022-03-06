0e1523d2ef71af191e3959498139c4441b2f7d28
FJ17-inchman <fjmrjr13@gmail.com>
2022-03-06 12:45:44 -0600

    Test p 3

---
+++ ViewBinding3/app/src/main/java/com/example/viewbinding3/BlankFragment.kt
@@ -0,0 +1,33 @@
+package com.example.viewbinding3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.viewbinding3.databinding.FragmentBlankBinding
+
+
+class BlankFragment : Fragment() {
+
+    private var _binding: FragmentBlankBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentBlankBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBoton.setOnClickListener { Toast.makeText(activity,"Estas en activity",Toast.LENGTH_SHORT).show() }
+    }
+
+
+}
--- ViewBinding3/app/src/main/java/com/example/viewbinding3/MainActivity.kt
+++ ViewBinding3/app/src/main/java/com/example/viewbinding3/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.viewbinding3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.viewbinding3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fb = BlankFragment()
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.acitvity_main, fb).commit()
+
+
     }
 }
